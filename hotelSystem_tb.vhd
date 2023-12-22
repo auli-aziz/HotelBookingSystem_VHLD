dc98cdd7943a9626f0a9a514968485a15c4da23c
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2023-12-22 21:28:06 +0700

    Menambahkan test bench

---
+++ hotelSystem_tb.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity hotelSystem_tb is
+end entity hotelSystem_tb;
+
+architecture rtl of hotelSystem_tb is
+    
+begin
+    
+    
+    
+end architecture rtl;
